d0307f05a791d63688bead056a7708beb531ef17
artemenko_av <artemenko_av@ARTEMENKO-A.synergenta.local>
2014-10-13 14:33:15 +0400

    Инициалзация - commit

---
+++ HelloGit/src/com/git/test/HelloGit.java
@@ -0,0 +1,16 @@
+package com.git.test;
+
+public class HelloGit {
+
+	public HelloGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello git 1");
+	}
+
+}
